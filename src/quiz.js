ec28b32dcfddd45fa5f7eece91d2e5f12e8310e5
Martina Ziza <martina.ziza@gmail.com>
2025-11-11 15:34:09 +0100

    day 2 done

--- src/quiz.js
+++ src/quiz.js
@@ -1,41 +1,61 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    constructor (questions, timeLimit, timeRemaining){
-        this.questions = questions
-        this.timeLimit = timeLimit
-        this.timeRemaining = timeRemaining
-        this.correctAnswers = 0
-        this.currentQuestionIndex = 0
+  // YOUR CODE HERE:
+  //
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
 
-    }
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-    getQuestion(){
-       
-    
-    return this.questions[this.currentQuestionIndex]
-     }
-    
-    moveToNextQuestion(){
-return this.currentQuestionIndex++
-    }
+  moveToNextQuestion() {
+    return this.currentQuestionIndex++;
+  }
 
-    shuffleQuestions(){
-    this.questions.sort(()=>Math.random()-0.5);
-    }
+  shuffleQuestions() {
+    this.questions.sort(() => Math.random() - 0.5);
+  }
 
-    checkAnswer(answer){
-    if (this.choices === this.answer){
-      return this.correctAnswers++
+  checkAnswer(answer) {
+    if (answer === this.questions[this.currentQuestionIndex].answer) {
+      this.correctAnswers++;
     }
+  }
 
-    }
-    
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    } else if (this.currentQuestionIndex === this.questions.length) return true;
+  }
 
-    hasEnded(){
-        if (this.currentQuestionIndex<this.questions.length){
-            return false
-        } else if (this.currentQuestionIndex=this.questions.length)
-            return true
+  filterQuestionsByDifficulty(difficulty) {
+    if (typeof difficulty !== "number" || difficulty < 1 || difficulty > 3) {
+      return 0;
     }
+    this.questions = this.questions.filter((questions) => {
+      return questions.difficulty === difficulty;
+    });
+
+    //  return this.questions.filter((object)=>{
+    // if (object.difficulty === difficulty){
+    //   return true;
+    // } else {
+    //   return false;
+    // }
+    //  })
+  }
+
+  averageDifficulty() {
+    const totDifficulty = this.questions.reduce((sum, obj) => {
+      return sum + obj.difficulty;
+    }, 0);
+    const avg = totDifficulty / this.questions.length;
+    return avg 
+  }
 }
+
